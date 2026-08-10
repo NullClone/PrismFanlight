2160d054822ff8197687878ab6232c1fe2720b52
Null <nullx7162@gmail.com>
2026-08-10 23:52:58 +0900

    Valueの折り畳みを非表示に

--- Editor/Timeline/FanlightTimelineClipAssetInspector.cs
+++ Editor/Timeline/FanlightTimelineClipAssetInspector.cs
@@ -10,6 +10,7 @@ namespace PrismFanlight.Editor
         // Fields
 
         private const string ScriptPropertyName = "m_Script";
+        private const string ValuePropertyName = "_value";
 
 
         // Methods
@@ -29,10 +30,29 @@ namespace PrismFanlight.Editor
 
                 if (property.propertyPath == ScriptPropertyName) continue;
 
+                if (property.propertyPath == ValuePropertyName)
+                {
+                    DrawChildren(property);
+                    continue;
+                }
+
                 EditorGUILayout.PropertyField(property, true);
             }
 
             serializedObject.ApplyModifiedProperties();
+        }
+
+        private static void DrawChildren(SerializedProperty property)
+        {
+            var child = property.Copy();
+            var end = child.GetEndProperty();
+            var enterChildren = true;
+
+            while (child.NextVisible(enterChildren) && !SerializedProperty.EqualContents(child, end))
+            {
+                enterChildren = false;
+                EditorGUILayout.PropertyField(child, true);
+            }
         }
     }
 }
